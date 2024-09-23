75d1df53bffdb3ea75460e9afe27d88e5b81267e
yanjin1225 <149980713+yanjin1225@users.noreply.github.com>
2024-09-23 16:23:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ def load_data():
 data = load_data()
 
 # %%
-price_filter = st.sidebar.slider('Select Median House Value Range', int(data['median_house_value'].min()), int(data['median_house_value'].max()), (50000, 300000))
+# %%
+st.markdown("## **CALIFORNIA HOUSING DATA**")  # 添加加粗大写标题
+price_filter = st.slider('Select Median House Value Range', int(data['median_house_value'].min()), int(data['median_house_value'].max()), (0, 500001))
+
 
 # 位置类型选择器
 ocean_proximity = st.sidebar.multiselect(
